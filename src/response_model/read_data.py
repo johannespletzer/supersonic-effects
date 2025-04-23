ac0ba24e08f7d6cd60f83615dc889c320bbb77ec
Johannes Pletzer <johannes.pletzer@posteo.de>
2025-04-23 16:31:14 +0200

    Remove absolute paths for data

--- src/response_model/read_data.py
+++ src/response_model/read_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import importlib.resources as pkg_resources
+from response_model import data
 
 
 def prepare_data(filepath):
@@ -20,27 +22,23 @@ def load_data(prepare=False, mode="Ozone"):
     """Load sensitivity and taylor data from file as a pandas DataFrame"""
 
     if mode not in ["Ozone", "Radiative_Forcing"]:
-        raise ValueError("mode keyword should be either 'Ozone' or 'Radiative_Forcing'")
+        raise ValueError("mode must be 'Ozone' or 'Radiative_Forcing'")
 
-    if prepare:
-        if mode == "Ozone":
-            df = prepare_data("./data/sensitivity_ozone.csv")
-            df_t = prepare_data("./data/taylor_param_ozone.csv")
-        elif mode == "Radiative_Forcing":
-            df = prepare_data("./data/sensitivity_radiative_forcing.csv")
-            df_t = prepare_data("./data/taylor_param_radiative_forcing.csv")
+    if mode == "Ozone":
+        sens_file = "sensitivity_ozone.csv"
+        taylor_file = "taylor_param_ozone.csv"
     else:
-        if mode == "Ozone":
-            df = pd.read_csv("./data/sensitivity_ozone.csv", sep=", ", engine="python")
-            df_t = pd.read_csv(
-                "./data/taylor_param_ozone.csv", sep=", ", engine="python"
-            )
-        elif mode == "Radiative_Forcing":
-            df = pd.read_csv(
-                "./data/sensitivity_radiative_forcing.csv", sep=", ", engine="python"
-            )
-            df_t = pd.read_csv(
-                "./data/taylor_param_radiative_forcing.csv", sep=", ", engine="python"
-            )
+        sens_file = "sensitivity_radiative_forcing.csv"
+        taylor_file = "taylor_param_radiative_forcing.csv"
+
+    with pkg_resources.files(data).joinpath(sens_file).open("r") as f1, \
+         pkg_resources.files(data).joinpath(taylor_file).open("r") as f2:
+
+        if prepare:
+            df = prepare_data(f1)
+            df_t = prepare_data(f2)
+        else:
+            df = pd.read_csv(f1, sep=", ", engine="python")
+            df_t = pd.read_csv(f2, sep=", ", engine="python")
 
     return df, df_t
